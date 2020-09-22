51848e40616eff9bd7932c195aea3edd910b0c2e
Anthony Leon <acidknights89@gmail.com>
2020-09-22 23:47:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,18 @@
 class Book
   def initialize(title)
-    
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
   end
 end
 Book.new("And Then There Were None")
